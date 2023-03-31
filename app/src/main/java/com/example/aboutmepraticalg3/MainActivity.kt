e2ae68ce35936a6b7806a072842e1e2f05baba6b
Veo <chingwh2003@gmail.com>
2023-03-31 14:57:21 +0800

    commit

--- app/src/main/java/com/example/aboutmepraticalg3/MainActivity.kt
+++ app/src/main/java/com/example/aboutmepraticalg3/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.aboutmepraticalg3
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutmepraticalg3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.doneButton.setOnClickListener{
+            addNickname(it)
+        }
+
+
+        binding.nicknameText.setOnClickListener {
+            updateNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+
+
+        binding.nicknameText.text = binding.nicknameEdit.text
+
+        binding.nicknameEdit.visibility = View.GONE
+        view.visibility = View.GONE
+        binding.nicknameText.visibility = View.VISIBLE
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE)
+                as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 6)
+    }
+
+    private fun updateNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+
+
+        binding.nicknameEdit.visibility = View.VISIBLE
+        binding.doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        editText.requestFocus()
     }
 }
